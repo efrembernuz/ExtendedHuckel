ba2bee35b6cbe1b4d01ed70c3451931a0a77f5a7
Abel Carreras <abelcarreras83@gmail.com>
2026-05-15 00:27:00 +0200

    add github action workflow

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 
 
 def get_version_number():
-    for l in open('huckelpy/__init__.py', 'r').readlines():
-        if not(l.find('__version__')):
-            exec(l, globals())
-            return __version__
+    main_ns = {}
+    for line in open('huckelpy/__init__.py', 'r').readlines():
+        if not(line.find('__version__')):
+            exec(line, main_ns)
+            return main_ns['__version__']
 
 
 setup(name='huckelpy',
       version=get_version_number(),
       description='Package to calculate the molecular orbitals with the extended huckel method',
       long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       author='Efrem Bernuz',
       author_email='komuisan@gmail.com',
-      packages=['huckelpy'],
+      packages=find_packages(where="."),
       package_data={'': ['basis_set.yaml']},
       include_package_data=True,
       install_requires=['numpy', 'PyYAML', 'scipy'])
